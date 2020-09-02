b067ae01581866125de5a49349e1f87c74357156
Adam McGurk <mcgurk@mcgurkconsulting.com>
2020-09-02 14:39:33 -0500

    Building in the tests for everything

--- cypress/integration/basic_navigation.js
+++ cypress/integration/basic_navigation.js
@@ -1,5 +1,9 @@
 describe("Basic Navigation Tests", () => {
 
+	beforeEach(() => {
+		cy.visit("/");
+	});
+
 	it("Opens the add entry screen", () => {
 
 		// making sure the entry screen is hidden
@@ -9,6 +13,40 @@ describe("Basic Navigation Tests", () => {
 		cy.get("#addNewEntry").trigger("touchstart");
 
 		cy.get("#addNewEntryScreen").should("not.have.attr", "aria-hidden");
+
+	});
+
+	it("Closes the add entry screen when clicking on the black background screen", () => {
+
+		// click the button to pop the entry screen open
+		cy.get("#addNewEntry").trigger("touchstart");
+
+		// CLOSES WHEN CLICKED ON THE BLACK BACKGROUND
+		cy.get("#opaqueBlackBackground").trigger("touchstart");
+		cy.get("#addNewEntryScreen").should("have.attr", "aria-hidden", "true");
+
+	});
+
+	it("Closes the add entry screen when clicking x", () => {
+
+		// click the button to pop the entry screen open
+		cy.get("#addNewEntry").trigger("touchstart");
+
+		// CLOSES WHEN CLICKED ON THE BLACK BACKGROUND
+		cy.get("#addNewEntryScreen header [data-close]").trigger("touchstart");
+		cy.get("#addNewEntryScreen").should("have.attr", "aria-hidden", "true");
+
+	});
+
+	it("Opens the side nav", () => {
+
+		// the nav is already closed
+		cy.get("#mainNav").should("have.attr", "aria-hidden", "true");
+
+		// touching the hamburger
+		cy.get("#navSwitcher").trigger("touchstart");
+
+		cy.get("#mainNav").should("not.have.attr", "aria-hidden");
 
 	});
 
