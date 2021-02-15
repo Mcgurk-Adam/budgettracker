7a594590d3e5d6ce6927bf16d5867a7aad1fe4dc
Adam McGurk <eldermcgurk@gmail.com>
2021-02-15 11:05:02 -0600

    Setting up the new head componenet and setting where the lang strings are

---
+++ view/html/includes/head.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="en-us">
+<head>
+
+	<meta charset="utf-8">
+	<title>Count Cash</title> <!-- lang-string -->
+
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link rel="preload" as="style" href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700&display=swap">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700&display=swap" media="print" onload="this.media='all'">
+	<noscript>
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700&display=swap">
+	</noscript>
+
+	<link href="/style/style.css" rel="stylesheet">
+
+	<link rel="icon" href="/favicon.ico">
+	<link rel="manifest" href="/manifest.webmanifest">
+	<meta name="theme-color" content="#38BEC9">
+	<link rel="apple-touch-icon" href="/assets/pwa/190icon.png">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="A small budget tracking app"> <!-- lang-string -->
+
+</head>
