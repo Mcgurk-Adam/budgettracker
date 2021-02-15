56213840eea00f1908e51bcbc6e79779426895fd
Adam McGurk <eldermcgurk@gmail.com>
2021-02-15 13:26:57 -0600

    Working on something here

--- tests/unit/dev/bash/ReadInputTest.php
+++ tests/unit/dev/bash/ReadInputTest.php
@@ -8,4 +8,12 @@ final class ReadInputTest extends TestCase {
 		$this->assertInstanceOf(ReadInput::class, new ReadInput('Test prompt'));
 	}
 
+	public function testSanitizationOfUserInput(): void {
+
+		$read_input = new ReadInput('Test prompt');
+
+		$reflected_class = new ReflectionClass($read_input);
+
+	}
+
 }
