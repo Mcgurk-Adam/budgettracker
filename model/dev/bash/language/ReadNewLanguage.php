5bc514a2ccb2b179469ea7c88e4bacd26a799092
Adam McGurk <eldermcgurk@gmail.com>
2021-02-15 15:12:33 -0600

    Fixed the regex

--- model/dev/bash/language/ReadNewLanguage.php
+++ model/dev/bash/language/ReadNewLanguage.php
@@ -3,14 +3,14 @@ declare(strict_types=1);
 
 final class ReadNewLanguage extends ReadInput {
 
-	private static $valid_regex = '/[a-z]{2}/';
+	private static $valid_regex = '/^[a-z]{2}$/';
 
 	public function __construct() {
 		parent::__construct('What is the two digit language code you would like to add?');
 	}
 
 	public function validate_input(): void {
-		if (empty($this->answer_string) || \preg_match(self::$valid_regex, $this->answer_string) === false) {
+		if (empty($this->answer_string) || !\preg_match(self::$valid_regex, $this->answer_string)) {
 			throw new BashInputException('Please provide valid input');
 		}
 	}
--- tests/unit/dev/bash/language/ReadNewLanguageTest.php
+++ tests/unit/dev/bash/language/ReadNewLanguageTest.php
@@ -9,11 +9,15 @@ final class ReadNewLanguageTest extends TestCase {
 	}
 
 	public function testRegex(): void {
+
+		// exposing the property
 		$read_input = new ReadNewLanguage();
 		$reflected_class = new ReflectionClass($read_input);
 		$property = $reflected_class->getProperty('valid_regex');
 		$property->setAccessible(true);
-		var_dump($property->getValue());
+
+		// we need to assertTrue the inverse of the return, because the expected return could either be false OR 0 . Both are falsy, but phpunit matches exactly false when calling assertFalse()
+		$this->assertTrue(!\preg_match($property->getValue(), 'english'));
 	}
 
 }
