9b0c7ef462d7a5ad587a2e289805672755735ef1
Adam McGurk <eldermcgurk@gmail.com>
2021-02-15 13:30:46 -0600

    I can now use protected methods in my tests!!!

--- tests/unit/dev/bash/ReadInputTest.php
+++ tests/unit/dev/bash/ReadInputTest.php
@@ -13,6 +13,10 @@ final class ReadInputTest extends TestCase {
 		$read_input = new ReadInput('Test prompt');
 
 		$reflected_class = new ReflectionClass($read_input);
+		$method = $reflected_class->getMethod('set_answer');
+		$method->setAccessible(true);
+		$method->invokeArgs($read_input, ['      needs Trimmed    ']);
+		$this->assertEquals($read_input->get_answer(), 'needs trimmed');
 
 	}
 
