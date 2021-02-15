16bf5c7e5dfb93c8ac593bced9c994e49b3c661f
Adam McGurk <eldermcgurk@gmail.com>
2021-02-15 17:16:55 -0600

    Changed file name and added one last test

--- tests/unit/dev/bash/TerminalOutputTest.php
+++ tests/unit/dev/bash/TerminalOutputTest.php
@@ -2,15 +2,19 @@
 declare(strict_types=1);
 use PHPUnit\Framework\TestCase;
 
-final class ReadInputTest extends TestCase {
+final class TerminalOutputTest extends TestCase {
 
 	public function testBuildsString(): void {
-		$this->assertString(TerminalOutput::build_string('This should be built just fine'));
+		$this->assertIsString(TerminalOutput::build_string('This should be built just fine'));
 	}
 
 	public function testThrowsExceptionOnInvalidColor(): void {
 		$this->expectException(TerminalOutputException::class);
 		TerminalOutput::get_bash_color_code('DOES NOT EXIST');
+	}
+
+	public function testGetsColorCode(): void {
+		$this->assertEquals(TerminalOutput::get_bash_color_code('WHITE'), 97);
 	}
 
 }
