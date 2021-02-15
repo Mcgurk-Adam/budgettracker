6acf2a7e0dc10a6f67c8cea6c13641a48ebb560e
Adam McGurk <eldermcgurk@gmail.com>
2021-02-15 13:31:48 -0600

    Some comments

--- tests/unit/dev/bash/ReadInputTest.php
+++ tests/unit/dev/bash/ReadInputTest.php
@@ -10,14 +10,17 @@ final class ReadInputTest extends TestCase {
 
 	public function testSanitizationOfUserInput(): void {
 
+		// init object
 		$read_input = new ReadInput('Test prompt');
 
+		// set accessibility of class method to be open just for the test and call it
 		$reflected_class = new ReflectionClass($read_input);
 		$method = $reflected_class->getMethod('set_answer');
 		$method->setAccessible(true);
 		$method->invokeArgs($read_input, ['      needs Trimmed    ']);
-		$this->assertEquals($read_input->get_answer(), 'needs trimmed');
 
+		// run test
+		$this->assertEquals($read_input->get_answer(), 'needs trimmed');
 	}
 
 }
