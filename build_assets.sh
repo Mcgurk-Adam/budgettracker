d2332b1767625210c40e9484ba71f0dde56174c9
Adam McGurk <amcgurk@bamboohr.com>
2021-02-19 16:43:43 -0600

    Added stuff to the client assets file

--- build_assets.sh
+++ build_assets.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-echo Building the client assets
+echo Build the client assets
+echo Building the HTML file
+php index.php > index.html
+echo Done building the HTML file
+echo Done building the client assets
