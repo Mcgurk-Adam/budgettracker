c411b186fb78f62bcfa306bbfbfdcdeb8239ce91
Adam McGurk <amcgurk@bamboohr.com>
2021-02-24 12:44:34 -0600

    Pretty much rewrote the add string file

--- dev/bin/lang/add-string.php
+++ dev/bin/lang/add-string.php
@@ -5,15 +5,14 @@ require_once 'vendor/autoload.php';
 $file_name_input = new ReadInput('What is the path to the file this string is held? (relative from /view/html/): ');
 $file_name_input->prompt();
 $file_name = $file_name_input->get_answer();
+while (empty($file_name)) {
+	$file_name_input->prompt();
+	$file_name = $file_name_input->get_answer();
+}
 
 // does the file exist?
-if (empty($file_name) || !\file_exists('view/html/'.$file_name)) {
+if (!\file_exists('view/html/'.$file_name)) {
 	echo TerminalOutput::build_string('We can\'t find that file.', 'RED');
-	$ending_paren = TerminalOutput::build_string(').', 'RED', false);
-	$php_command = TerminalOutput::build_string('php dev/bin/lang/add-string.php', 'WHITE', false);
-	echo TerminalOutput::build_string('Make sure you run this script from the root of the repo (so your command should look like this: '.$php_command.$ending_paren, 'RED');
-	$file_path = TerminalOutput::build_string('includes/your-file.php', 'WHITE', false);
-	echo TerminalOutput::build_string('Additionally, make sure your file path is realtive to the path /view/html (so if your file is at /view/html/includes/your-file.php, your input should read: '.$file_path.$ending_paren, 'RED');
 	TerminalOutput::exit_script(1);
 }
 
@@ -71,14 +70,46 @@ foreach ($all_languages as &$language_data) {
 		$language_data = $language_string_file->get_decoded_json();
 		continue;
 	}
-	foreach ($language_string_file->get_decoded_json() as $file_path => $data) {
+	$template_file_data = $language_string_file->get_decoded_json();
+	// getting all necessary strings and templates in there if they exist or not
+	foreach ($template_file_data as $file_path => $data) {
 		if (empty($language_data[$file_path])) {
 			$language_data[$file_path] = $data;
 			continue;
-		} else {
-			// it does exist, we're gonna have to go deeper
+		}
+		// it does exist, we're gonna have to go deeper
+		foreach ($data as $ele => $attribute_data) {
+			if (empty($language_data[$file_path][$ele])) {
+				$language_data[$file_path][$ele] = $attribute_data;
+				continue;
+			}
+			foreach ($attribute_data as $attribute_name => $attribute_value) {
+				$desired_attribute_to_add = (\strtolower($attribute_answer) === 'y' ? $attribute_name->get_answer() : 'innerText');
+				$language_data[$file_path][$ele][$desired_attribute_to_add] = '';
+
+			}
 		}
 	}
+
+	// cleaning up the file from anything that happened outside of the APPROVED way of happening
+	foreach ($language_data as $file_path => $file_elements) {
+		if (empty($template_file_data[$file_path])) {
+			unset($language_data[$file_path]);
+			continue;
+		}
+		foreach ($file_elements as $ele_path => $string_data) {
+			if (empty($template_file_data[$file_path][$ele_path])) {
+				unset($language_data[$file_path][$ele_path]);
+				continue;
+			}
+			foreach ($string_data as $name => $value) {
+				if (empty($template_file_data[$file_path][$ele_path][$name])) {
+					unset($language_data[$file_path][$ele_path][$name]);
+				}
+			}
+		}
+	}
+
 }
 $all_languages_file->write_to_file(json_encode($all_languages));
 $all_languages_file->beautify_file();
